7aa74bd6c53110845d5c3057a2bcaa018fb9d67b
Ricardo Souza Morais <ricardo.souza.morais@gmail.com>
2019-12-03 23:31:51 -0300

    Ajustando rota padrão

--- spa/src/app/app-routing.module.ts
+++ spa/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'customer', pathMatch: 'full' },
+  { path: '', redirectTo: 'customers', pathMatch: 'full' },
   { path: 'customers', component: CustomerListComponent },
   { path: 'details/:id', component: CustomerDetailsComponent },
 ];

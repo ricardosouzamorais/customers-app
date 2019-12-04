daf5b86da5be10c8016d73c9b6f61392c7d0787a
Ricardo Souza Morais <ricardo.souza.morais@gmail.com>
2019-12-03 21:46:10 -0300

    Geração da imagem do backend por meio do Maven

--- microservice/src/main/docker/run.sh
+++
@@ -1,5 +0,0 @@
-echo "********************************************************"
-echo "Starting Template Server with Docker "
-echo "********************************************************"
- 
-java -jar /app/@project.build.finalName@.jar
